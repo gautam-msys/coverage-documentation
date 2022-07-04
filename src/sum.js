abad5e06e5d80471ccd2ff42d53ac6643d502f26
gautam-msys <gkumar@msystechnologies.com>
2022-07-04 21:44:05 +0530

    added one more func

--- src/sum.js
+++ src/sum.js
@@ -10,7 +10,11 @@ function sum3(num1, num2) {
   return num1 + num2;
 }
 
+function sum4(num1, num2) {
+  return num1 + num2;
+}
 
 module.exports = sum;
 module.exports = sum2;
 module.exports = sum3;
+module.exports = sum4;
